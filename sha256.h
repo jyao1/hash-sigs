54b4e5305aca136c796df0db9f81a6c42bb4b65e
Jiewen Yao <jiewen.yao@intel.com>
2020-03-22 14:03:30 +0800

    Remove openssl dependency.
    Signed-off-by: jiewen.yao@intel.com

--- sha256.h
+++ sha256.h
@@ -1,7 +1,7 @@
 #if !defined(SHA256_H_)
 #define SHA256_H_
 
-#define USE_OPENSSL 1   /* We use the OpenSSL implementation for SHA-256 */
+#define USE_OPENSSL 0   /* We use the OpenSSL implementation for SHA-256 */
                         /* (which is quite a bit faster than our portable */
                         /* C version) */
 
